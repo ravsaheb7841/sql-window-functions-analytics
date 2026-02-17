a5e21d358a33d6800a3dad1353e73e33eec0b01e
ravsaheb7841 <bansoderav@gmail.com>
2026-02-17 22:31:30 +0530

    Added dataset and project structure

---
+++ Day-1/2_salary_ranking.sql
@@ -0,0 +1,28 @@
+/*
+-- Question:
+Rank employees based on their Monthly Salary in descending order (highest first).
+*/
+
+/*
+-- Query:
+*/
+SELECT 
+    No,
+    CONCAT(First_Name, ' ', Last_Name) AS Name,
+    Department,
+    Country,
+    Monthly_Salary,
+    Annual_Salary,
+    RANK() OVER(ORDER BY Monthly_Salary DESC) AS emp_rank
+FROM employees.employees;
+
+/*
+-- Sample Output:
+
+| No   | Name              | Department             | Country       | Monthly_Salary | Annual_Salary | emp_rank |
+|------|------------------|----------------------|---------------|----------------|---------------|----------|
+| 325  | Ashraf Aleid      | Account Management    | Egypt         | 3450           | 41400         | 1        |
+| 348  | Dalia Hamdan      | Major Mfg Projects    | Egypt         | 3446           | 41352         | 2        |
+| 317  | Muhamad Almisri   | Quality Control       | Saudi Arabia  | 3443           | 41316         | 3        |
+| 324  | Muhamad Salhany   | Quality Control       | Egypt         | 3428           | 41136         | 4        |
+*/
